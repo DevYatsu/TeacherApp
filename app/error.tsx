506eb0e17ec3beecba72c68bc2459491482af26d
Yatsu <yatsu.dev@gmail.com>
2024-07-17 00:32:13 +0200

    meta data add to error pages

--- app/error.tsx
+++ app/error.tsx
@@ -1,4 +1,11 @@
 import { ErrorPage } from "@/components/error-page";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Error Page",
+  description:
+    "An error has occurred. Please check the URL or try again later.",
+};
 
 export default function Page() {
   return <ErrorPage />;
--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,4 +1,11 @@
 import { PageNotFound } from "@/components/404-page";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "404 | Not Found",
+  description:
+    "The page you are looking for does not exist. Please check the URL or return to the homepage.",
+};
 
 export default function Page() {
   return <PageNotFound />;
